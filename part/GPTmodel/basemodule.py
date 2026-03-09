ed7d3da5247a0583b9cb8d04a706928d21c8f25d
LJK <dlwhdrlf0619@naver.com>
2026-03-09 23:35:41 +0900

    feat: Base module
    - Add config
    - Prepare base module

--- part/GPTmodel/basemodule.py
+++ part/GPTmodel/basemodule.py
@@ -5,3 +5,38 @@ import torch.nn as nn
 class BaseGPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_out = nn.Dropout(cfg["drop_rate"])
+        self.trf_blocks = nn.Sequential(
+            *[BaseTransformerBlock(cfg) for _ in range(cfg["n_layers"])]
+        )
+        self.final_norm = BaseLayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
+
+    def forward(self, x):
+        batch_size, seq_len = x.shape
+        tok_embeds = self.tok_emb(x)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=x.device))
+        x = tok_embeds + pos_embeds
+        x = self.drop_out(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
+
+class BaseTransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+
+    def forward(self, x):
+        return x
+
+
+class BaseLayerNorm(nn.Module):
+    def __init__(self, normalized_shape, eps=1e-5):
+        super().__init__()
+
+    def forward(self, x):
+        return x
